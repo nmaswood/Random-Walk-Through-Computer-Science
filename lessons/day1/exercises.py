fa35d146fe20d48c85cdb4f76cf23477beee30aa
Nasr Maswood <nmaswood@uchicago.edu>
2017-07-05 15:21:51 -0400

    Foog

--- lessons/day1/exercises.py
+++ lessons/day1/exercises.py
@@ -1,2 +1,17 @@
-def foo():
-    bar
+# 1.0
+
+"""
+
+print the following things to the screen
+
+"hello world"
+1 + 1 / 2
+(1+ 1) /2
+1+ 1
+"1" + "1"
+
+"""
+
+# Write your code here
+
+print ("testing")
