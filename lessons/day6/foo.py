eb99384f5f9fc5f50b5e4bb7c493c12684b53c5e
Nasr Maswood <nmaswood@uchicago.edu>
2017-07-18 12:12:43 -0400

    Foo

--- lessons/day6/foo.py
+++ lessons/day6/foo.py
@@ -11,29 +11,31 @@
 
 import pandas as pd
 
-
-df = pd.DataFrame(
-    {'fuck': [0,1,2],
-   'year_month': ['2016-12-01', '2016-12-02','2016-12-03']})
+df = pd.DataFrame({
+    'random': [1234,4567,902],
+   'year_month': ['2013-12-01', '2016-12-01','2012-12-01']
+})
 
 def filter_data(df, start, end):
 
     """
-    This function takes a data frame and returns a one where
-    the column year_month only has dates in between the range
-    of start and end
+    Takes df
+
+    * Sorts it
+    * Removes dates in the range (start, end) specified
+    * Returns new df
 
     This function does not modify the original data but creates a new data frame.
-
 
     """
 
     df2 = df.copy()
+    df2 = df2.sort_values('year_month')
     df2['year_month'] = pd.to_datetime(df['year_month'])
     df2 = df2[(df2['year_month'] < end) & (df2['year_month'] > start)]
 
     return df2
 
-new_df = filter_data(df,'2016-12-01', '2016-12-03' )
+new_df = filter_data(df,'2013-11-01', '2017-12-01' )
 
 print (new_df)
