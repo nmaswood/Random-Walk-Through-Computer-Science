22baebfa26763dc6d07e84471714b0d648bfb9a9
Nasr Maswood <nmaswood@uchicago.edu>
2017-07-23 23:00:02 -0400

    foo

---
+++ lessons/day10/exercises.py
@@ -0,0 +1,25 @@
+def dice_simulator(n_sides):
+
+    """
+    Write a function that simulates a fair dice with n_sides sides
+    """
+
+    return 0
+
+def calculator_app():
+
+    """
+
+    Design a program that runs in your terminal. It should ask the user for two numbers
+    and support +, -, * operations between them.
+
+    """
+    return
+
+def text_adventure_game():
+
+    """
+    Use the terminal to build a text adventure game. Creativity is encouraged here.
+    """
+
+    return
