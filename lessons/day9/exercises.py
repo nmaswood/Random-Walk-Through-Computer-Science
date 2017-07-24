8db7f82bb62b07c3e65317ea21d62e03b628a3df
Nasr Maswood <nmaswood@uchicago.edu>
2017-07-23 22:49:33 -0400

    foo

---
+++ lessons/day9/exercises.py
@@ -0,0 +1,45 @@
+def double_and_add_three_impure(l):
+
+    """
+    perform (2 * x + 3) on every element of l
+
+    Modify the original structure
+    """
+
+    return 
+
+def double_and_add_three_pure(l):
+
+    """
+    perform (2 * x + 3) on every element of l
+
+    Do not Modify the original structure
+    """
+
+    return []
+
+def special_function(input_string):
+
+    """
+    Given a string remove all words that begin with a vowel (a,e,i,o,u)
+    Uppercase all letters that do not begin with a vowel
+
+    """
+
+    return ''
+
+
+def fibonacci_generator(n):
+
+
+    """
+
+    Create a generator the yields the numbers in the fibonacci up to the nth term
+    """
+
+    yield
+
+# Create a Carclass
+# An has three attributes model, year, cost
+# Give the cost three methods of your choice e.g. 
+# drive, park, etc.
